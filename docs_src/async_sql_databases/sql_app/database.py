bfef36761b612b1244ee2882a9d8866218df7d9a
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-11-09 16:01:28 +0200

    Use declarative_base to sqlalchemy.orm
    https://docs.sqlalchemy.org/en/14/changelog/migration_20.html#declarative-becomes-a-first-class-api

--- docs_src/async_sql_databases/sql_app/database.py
+++ docs_src/async_sql_databases/sql_app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://user:password@postgresserver/db"
