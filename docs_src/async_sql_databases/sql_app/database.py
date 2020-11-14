0eb012ea175a4be0d0258293288e2568719c8984
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-11-14 20:52:53 +0200

    Include connect_args in database.py

--- docs_src/async_sql_databases/sql_app/database.py
+++ docs_src/async_sql_databases/sql_app/database.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import declarative_base
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://user:password@postgresserver/db"
 
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 Base = declarative_base()
